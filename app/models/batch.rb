593415e8de6f12202fe78425d5e2da36292fbc13
João Fagundes <joaovictordmf@gmail.com>
2020-07-17 22:22:05 -0300

    add validations to models

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,3 +1,6 @@
 class Batch < ApplicationRecord
   has_many :orders
+
+  validates :reference, presence: true, uniqueness: { case_sensitive: false }
+  validates :purchase_channel, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,15 @@
 class Order < ApplicationRecord
   STATUSES = %i[ready production closing sent]
 
-  belongs_to :batch
+  belongs_to :batch, optional: true
+
+  validates :reference, presence: true, uniqueness: { case_sensitive: false }
+  validates :purchase_channel, presence: true
+  validates :address, presence: true
+  validates :delivery_service, presence: true
+  validates :total_value, presence: true, numericality: { greater_than: 0, only_integer: false }
+  validates :batch, presence: true, if: -> { !ready? }
+  validates :batch, absence: true, if: -> { ready? }
 
   enum status: STATUSES
 end
