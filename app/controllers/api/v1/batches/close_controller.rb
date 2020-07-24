65b64fdcc7ac8418eaceb55bc9c89c1609e0c542
João Fagundes <joaovictordmf@gmail.com>
2020-07-23 23:15:15 -0300

    add batches close action and spec

---
+++ app/controllers/api/v1/batches/close_controller.rb
@@ -0,0 +1,34 @@
+module API
+  module V1
+    module Batches
+      class CloseController < BaseController
+        def create
+          if @batch.present?
+            if batch_status_updater_service.call
+              render status: :ok,
+                     json: { success: true }
+            else
+              render status: :unprocessable_entity,
+                     json: {
+                       errors: batch_status_updater_service.errors.full_messages,
+                       success: false
+                     }
+            end
+          else
+            render status: :not_found,
+                   json: {
+                     errors: "Batch not found",
+                     success: false
+                   }
+          end
+        end
+
+        private
+
+        def batch_status_updater_service
+          @batch_status_updater_service ||= ::Batches::StatusUpdater.new(@batch, :closing)
+        end
+      end
+    end
+  end
+end
---
+++ spec/acceptance/api/v1/batches/close_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+
+describe "V1 - Batches - Close", type: :request do
+  describe "create" do
+    let(:batch) { create :batch }
+    let!(:orders) { create_list :order, 3, status: :production, batch: batch }
+
+    shared_examples "correct status code and data success" do |status, success|
+      before { post "/v1/batches/#{try(:batch_reference) || batch.reference}/close", params: params }
+
+      it "status code is #{status}" do
+        expect(response).to have_http_status(status)
+      end
+
+      it "data success is #{success}" do
+        expect(JSON.parse(response.body)["success"]).to eq(success)
+      end
+    end
+
+    shared_examples "correct error message" do |message|
+      it { expect(JSON.parse(response.body)["errors"]).to include(message) }
+    end
+
+    context "success" do
+      let(:params) { { delivery_service: orders.sample.delivery_service } }
+
+      include_examples "correct status code and data success", :ok, true
+    end
+
+    context "failure" do
+      let(:params) { { delivery_service: orders.sample.delivery_service } }
+
+      context "reference from inexistent batch" do
+        include_examples "correct status code and data success", :not_found, false do
+          let(:batch_reference) { "inexistent_batch123" }
+        end
+
+        include_examples "correct error message", "Batch not found"
+      end
+
+      context "orders are not in production status" do
+        let(:params) { { delivery_service: orders.sample.delivery_service } }
+
+        before { orders.each {|order| order.update_attribute(:status, :sent) } }
+
+        include_examples "correct status code and data success", :unprocessable_entity, false
+
+        include_examples "correct error message", "Can't change from sent to closing."
+      end
+    end
+  end
+end
